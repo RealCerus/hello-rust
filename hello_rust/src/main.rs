b964aff8169bc2d6d616ce4e65538ba523d70ee2
Max <m.dorn.2003@web.de>
2019-11-13 09:41:03 +0000

    Added time print

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,11 @@
+use std::time;
+
 fn main() {
-    println!("Hello, world!");
+    print_time_in_millis();
+}
+
+fn print_time_in_millis() {
+    let now = time::SystemTime::now();
+    let now_in_ms = now.duration_since(time::UNIX_EPOCH).expect("WTF").as_millis();
+    println!("{}", now_in_ms);
 }
